4c2cc716c97c1d99d51a69165c7532043b072878
Malik Umair Khalid <fighteromega@gmail.com>
2021-06-30 03:15:03 +0500

    3 Questions Completed

---
+++ app.js
@@ -0,0 +1,55 @@
+// // Suppose You have an array of object
+
+// var itemsArray = [
+// {name:"juice",price:50, quantity:3},
+// {name:"cookie",price:30, quantity:9},
+// {name:"shirt",price:880, quantity:1},
+// {name:"pen",price:100, quantity: 2}
+// ];
+// console.log(itemsArray.price)
+// for (let i = 0; i < itemsArray.length; i++) {
+//      var price = itemsArray[i].price
+//     var quantity = itemsArray[i].quantity
+//     itemsArray[i].totalAmmount = price * quantity
+//     console.log(itemsArray)
+// }
+
+// // Create an object with properties name, email, password, age,
+// // gender, city, country.
+
+// var details = {
+//     firstName: "Umair",
+//     LastName: "khalid",
+//     email: 'fighteromega@gmail.com',
+//     passward: 'asdfas',
+//     age: 19,
+//     gender: 'Male',
+//     city: 'karachi',
+//     country: 'pakistan'
+// }
+// console.log(details.firstName)
+// console.log(details.LastName)
+// console.log(details.country)
+// console.log(details.age)
+
+
+
+// // 3
+
+
+// function Student(name,classa,section,id){
+//     this.name = name;
+//     this.class = classa;
+//     this.section = section;
+//     this.id = id
+// }
+// var studentsDetails = [
+//     new Student ("umair",12,"A",3123),
+//     new Student("Adil",12,"A",23123),
+//     new Student("baba",12,"A",23123),
+//     new Student("Kaka",12,"A",23123),
+//     new Student("Nana",12,"A",23123),
+// ]
+// console.log(studentsDetails)
+
+
