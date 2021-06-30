aad8c6081c0fbe74077f5ebf29c0f3af02c41bf0
Malik Umair Khalid <fighteromega@gmail.com>
2021-07-01 01:49:23 +0500

    aligmnebts

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-                                        // // Question One
+// // Question One
 
 // var itemsArray = [
-// {name:"juice",price:50, quantity:3},
-// {name:"cookie",price:30, quantity:9},
-// {name:"shirt",price:880, quantity:1},
-// {name:"pen",price:100, quantity: 2}
+//     { name: "juice", price: 50, quantity: 3 },
+//     { name: "cookie", price: 30, quantity: 9 },
+//     { name: "shirt", price: 880, quantity: 1 },
+//     { name: "pen", price: 100, quantity: 2 }
 // ];
 // console.log(itemsArray.price)
 // for (let i = 0; i < itemsArray.length; i++) {
-//      var price = itemsArray[i].price
+//     var price = itemsArray[i].price
 //     var quantity = itemsArray[i].quantity
 //     itemsArray[i].totalAmmount = price * quantity
 //     console.log(itemsArray)
 // }
 
-//                                         // Question Two
+// // Question Two
 
 // var details = {
 //     firstName: "Umair",
@@ -33,34 +33,34 @@
 
 
 
-                                        // // Question Three
+// // Question Three
 
 
-// function Student(name,classa,section,id){
+// function Student(name, classa, section, id) {
 //     this.name = name;
 //     this.class = classa;
 //     this.section = section;
 //     this.id = id
 // }
 // var studentsDetails = [
-//     new Student ("umair",12,"A",3123),
-//     new Student("Adil",12,"A",23123),
-//     new Student("baba",12,"A",23123),
-//     new Student("Kaka",12,"A",23123),
-//     new Student("Nana",12,"A",23123),
+//     new Student("umair", 12, "A", 3123),
+//     new Student("Adil", 12, "A", 23123),
+//     new Student("baba", 12, "A", 23123),
+//     new Student("Kaka", 12, "A", 23123),
+//     new Student("Nana", 12, "A", 23123),
 // ]
 // console.log(studentsDetails)
 
 
 
 
-//                                         //  Question Four 
+// //  Question Four 
 
 
-// function submit(){
+// function submit() {
 //     // Getting Values
 //     var naMe = document.getElementById("name")
-//     naMe = naMe.value 
+//     naMe = naMe.value
 //     var address = document.getElementById("address")
 //     address = address.value
 //     var education = document.getElementById("Edu")
@@ -68,33 +68,33 @@
 
 //     var gender = document.getElementsByName("gender")
 //     for (let i = 0; i < gender.length; i++) {
-//         if(gender[i].checked){
-//             var gender =(gender[i].value)
-//         }    
+//         if (gender[i].checked) {
+//             var gender = (gender[i].value)
+//         }
 //     }
 
 //     // Setting Values In Object
 
-// function Details(naMe,gender,education,address){
-//     this.name = naMe;
-//     this.gender = gender;
-//     this.education = education;
-//     this.address = address
-// }
-// var studentsDetails = [
-//    new Details(naMe,gender,education,address)
-// ]
-// console.log(studentsDetails)
+//     function Details(naMe, gender, education, address) {
+//         this.name = naMe;
+//         this.gender = gender;
+//         this.education = education;
+//         this.address = address
+//     }
+//     var studentsDetails = [
+//         new Details(naMe, gender, education, address)
+//     ]
+//     console.log(studentsDetails)
 
-// //  Reseting Form
+//     //  Reseting Form
 
-// var naMe = document.getElementById("name")
-// var address = document.getElementById("address")
-// var education = document.getElementById("Edu")
-// naMe.value  = ""
-// address.value = ""
-// education.value = ""
-// gender.value = ""
+//     var naMe = document.getElementById("name")
+//     var address = document.getElementById("address")
+//     var education = document.getElementById("Edu")
+//     naMe.value = ""
+//     address.value = ""
+//     education.value = ""
+//     gender.value = ""
 
 // }
 
